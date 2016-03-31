cbfc94943ddb51d03b4c6babf443ab760ab0fa0a
ALEXANDRA ARGUIMBAU <AARGUIMBAU@ALEXANDRAs-Air.fios-router.home>
2016-03-31 02:55:21 -0400

    new tic tac toe game

--- tic_tac_toe/scripts.js
+++ tic_tac_toe/scripts.js
@@ -1,34 +1,50 @@
 console.log('loaded');
 //setting document up to fire after loading
 $(document).ready(function(){
-// USER STORY
+
+
+// write USER STORY
   //change to player counter
 //indexing individual square
+var ticTacToe = {};
+var player = 0;
+$('#boardSetUp').click('td', function(e){
+  var $td = $(e.currentTarget);
+});
+var currentPlayer = 0;
 
-
-
-
-
-
-
-var userTurn = 0;
-
-
-$(".cOne, .cTwo, .cThree").click(function(){
+$('td').click(function(){
     var currentCell = $(this);
-    if(userTurn == 0){
-      userTurn = 1;
-
-
-      currentCell.css('background', 'url(images/image1.jpg)');
+    if(player == 0){
+      player = 1;
+      return currentCell.css('background', 'url(images/image3.jpg)');
     } else {
-      userTurn = 0;
-  
-      currentCell.css('background', 'url(images/image2.jpg)')
-    }      //FIND OTHER IMAGES!!!
+      player = 0;
+    return currentCell.css('background', 'url(images/image2.jpg)')
+    }
 
 
 })
-
-
 });
+
+// var usedCell = {
+//
+// }
+//   if(player == winner)
+// //winning matches
+//
+// var possibleWins =
+// ['c1','c4','c7'],
+// ['c2','c5','c8'],
+// ['c3','c6','c9'],
+// ['c3','c5', 'c7'],
+// ['c1','c5','c9']
+//
+// function findWinner(possibleWins){
+//   var winner = {};
+//   for (var i = 0; i < possibleWins.length; i++) {
+//     console.log(possibleWins[i]);
+//   }
+//
+
+//
